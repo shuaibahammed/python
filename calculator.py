5b4ed68ac23e4fd3552498d8d8b4e24784e5f233
Shuaib ahammed <shuaibek@gmail.com>
2020-07-16 12:04:51 +0530

    buttons added

---
+++ calculator.py
@@ -0,0 +1,33 @@
+from tkinter import *
+
+window = Tk()
+
+window.geometry("200x200")
+
+# creating button
+button1 = Button(text="ok", bg="blue", fg="yellow")
+button2 = Button(text="ok", bg="blue", fg="yellow")
+button3 = Button(text="ok", bg="blue", fg="yellow")
+button4 = Button(text="ok", bg="blue", fg="yellow")
+button5 = Button(text="ok", bg="blue", fg="yellow")
+button6 = Button(text="ok", bg="blue", fg="yellow")
+button7 = Button(text="ok", bg="blue", fg="yellow")
+button8 = Button(text="ok", bg="blue", fg="yellow")
+button9 = Button(text="ok", bg="blue", fg="yellow")
+button10 = Button(text="ok", bg="blue", fg="yellow")
+button11 = Button(text="ok", bg="blue", fg="yellow")
+button12= Button(text="ok", bg="blue", fg="yellow")
+button13= Button(text="ok", bg="blue", fg="yellow")
+button14= Button(text="ok", bg="blue", fg="yellow")
+button15= Button(text="ok", bg="blue", fg="yellow")
+button16= Button(text="ok", bg="blue", fg="yellow")
+button17= Button(text="ok", bg="blue", fg="yellow")
+
+
+
+button.pack()
+
+window.mainloop()
+
+
+
